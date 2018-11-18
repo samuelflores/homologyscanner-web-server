221e7e23553798ab2964084829a377ccb0e61b5d
Samuel Flores <sflores@ae15a.dyn.scilifelab.se>
2018-11-18 03:30:06 +0100

    Added several directories. Leaving out LIMS_Assignment/ and a few other items that do not seem necessary.

---
+++ includes/connection.inc.php
@@ -0,0 +1,31 @@
+<?php
+$host="localhost";
+$username="mmbcgi";
+$password="mMB¢9IUser";
+$database="lims_project";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+//magic quotes logic
+if (get_magic_quotes_gpc()) {
+	function stripslashes_deep($value)
+	{
+		$value = is_array($value) ?
+		array_map('stripslashes_deep', $value) :
+		stripcslashes($value);
+		return $value;
+	}
+	
+	$_POST = array_map('stripslashes_deep', $_POST);
+	$_GET = array_map('stripslashes_deep', $_GET);
+	$_COOKIE = array_map('stripslashes_deep', $_COOKIE);
+	$_REQUEST = array_map('stripslashes_deep', $_REQUEST);
+}
+?>
+
