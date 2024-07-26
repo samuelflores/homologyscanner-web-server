07c1e4972f77e644f09b283dcbd99f6f2ea86f56
samuelflores <samuelfloresc@gmail.com>
2024-07-26 18:13:10 +0200

    make generalConfig.php contain coderootdirectory, and include coderootdirectory.dtypes.php. dtypes.php cannot lie directly in the web directory as it contains passwords.

--- generalConfig.php
+++ generalConfig.php
@@ -1,10 +1,9 @@
 <?php
+error_log("check 50");
 define('myLibPath' , "/usr/lib/x86_64-linux-gnu/blas:/home/sam/svn/breeder/build:/usr/local/lib"); // libs for homologyScanner, to be used for running jobs, either in SLURM or directly in the foreground. "define" is superior in this case to simply assigning wiht "=" because the former is a constant that is defined globally. 
-//$myLibPath = "/usr/lib/x86_64-linux-gnu/blas:/home/sam/svn/breeder/build:/usr/local/lib";
+error_log("check 51");
 define('coderootdirectory','/home/sam/github/homologyscanner-web-server'); // This needs to be separate from webrootdirectory, because it will hold the logins and passwords for mysql. Coul    d not define in dtypes.php, because that is precisely one of the files that needs to be hidden from the web accessible directories.
-include ('/home/sam/github/homologyscanner-web-server/dtypes.php'); // location of dtypes.php, which in turn has the not-safe config variables.
+error_log("check 52");
 include (coderootdirectory."/dtypes.php"); // location of dtypes.php, which in turn has the not-safe config variables.
-   
-define('administratorEmail', 'samuelfloresc@gmail.com');
-#define('administratorEmail', 'samuel.flores@scilifelab.se');
+error_log("check 55");
 ?>
